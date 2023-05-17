4c809a27dc1b6abbda43b58d6c5d4c72e08d0cb3
RCJ <34693840+R-Crane@users.noreply.github.com>
2023-05-16 17:02:57 -0700

    Delete Javadoc Comments directory

--- Javadoc Comments/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"c195Project"},{"l":"c195Project.controller"},{"l":"c195Project.DAO"},{"l":"c195Project.model"},{"l":"c195Project.util"}];updateSearchResults();
